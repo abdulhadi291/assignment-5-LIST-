0afcb311aa9c0a3e53a3ffcc462df13a65818270
Abdul Hadi <125742871+abdulhadi291@users.noreply.github.com>
2025-05-03 22:54:14 +0500

    question 2

--- assignment.dart
+++ assignment.dart
@@ -1,7 +1,24 @@
 // Assignment 5
 
 // Q1
+// void main() {
+//   List names = ['Ali', 'Ahmed', 'Hadi'];
+//   print(names);
+// }
+
+//---------------------------------------------------------------------------------
+
+// Q2
 void main() {
-  List names = ['Ali', 'Ahmed', 'Hadi'];
-  print(names);
+  List days = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+  ];
+
+  print(days[6]);
 }
