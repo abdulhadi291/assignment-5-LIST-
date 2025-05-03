5127bf1594951206fee54ab5ee0f17da9f40afa4
Abdul Hadi <125742871+abdulhadi291@users.noreply.github.com>
2025-05-03 23:12:32 +0500

    question 5

--- assignment.dart
+++ assignment.dart
@@ -41,13 +41,25 @@
 //---------------------------------------------------------------------------------
 
 // Q4
+// void main() {
+//   List numbers = [10, 5, 23, 1, 45, 8];
+//   numbers.sort();
+
+//   print(numbers);
+
+//   print("smallest number is: ${numbers[0]}");
+
+//   print("largest number is: ${numbers[5]}");
+// }
+
+//---------------------------------------------------------------------------------
+
+// Q5
 void main() {
-  List numbers = [10, 5, 23, 1, 45, 8];
+  List numbers = [4, 17, 9, 22, 13, 6];
+
   numbers.sort();
 
   print(numbers);
-
-  print("smallest number is: ${numbers[0]}");
-
-  print("largest number is: ${numbers[5]}");
+  print("maximum value is ${numbers[5]}");
 }
