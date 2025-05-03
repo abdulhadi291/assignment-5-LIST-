ab3013d3001d99e299a9e539c33607e445f7c0c6
Abdul Hadi <125742871+abdulhadi291@users.noreply.github.com>
2025-05-03 23:08:44 +0500

    question 4

--- assignment.dart
+++ assignment.dart
@@ -26,14 +26,28 @@
 //---------------------------------------------------------------------------------
 
 // Q3
-void main() {
-  Map student = {
-    'name': 'Hadi',
-    'class': '10th',
-    'rollNo': 3,
-    'grade': 'A+',
-    'percentage': 92.3
-  };
+// void main() {
+//   Map student = {
+//     'name': 'Hadi',
+//     'class': '10th',
+//     'rollNo': 3,
+//     'grade': 'A+',
+//     'percentage': 92.3
+//   };
 
-  print(student);
+//   print(student);
+// }
+
+//---------------------------------------------------------------------------------
+
+// Q4
+void main() {
+  List numbers = [10, 5, 23, 1, 45, 8];
+  numbers.sort();
+
+  print(numbers);
+
+  print("smallest number is: ${numbers[0]}");
+
+  print("largest number is: ${numbers[5]}");
 }
