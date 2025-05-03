f8cb23f36a62c4669ab573733126a6d3a1147022
Abdul Hadi <125742871+abdulhadi291@users.noreply.github.com>
2025-05-03 23:02:04 +0500

    question 3

--- assignment.dart
+++ assignment.dart
@@ -9,16 +9,31 @@
 //---------------------------------------------------------------------------------
 
 // Q2
-void main() {
-  List days = [
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-    'Sunday'
-  ];
+// void main() {
+//   List days = [
+//     'Monday',
+//     'Tuesday',
+//     'Wednesday',
+//     'Thursday',
+//     'Friday',
+//     'Saturday',
+//     'Sunday'
+//   ];
 
-  print(days[6]);
+//   print(days[6]);
+// }
+
+//---------------------------------------------------------------------------------
+
+// Q3
+void main() {
+  Map student = {
+    'name': 'Hadi',
+    'class': '10th',
+    'rollNo': 3,
+    'grade': 'A+',
+    'percentage': 92.3
+  };
+
+  print(student);
 }
